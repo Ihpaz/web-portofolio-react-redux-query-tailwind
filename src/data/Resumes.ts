1b77ba8d60d6d72755efcb5bb33c733cb2c4b6e6
ihfazm <ihfazm@gmail.com>
2026-01-21 08:59:09 +0700

    feat: Add portfolio project data and associated image assets.

--- src/data/Resumes.ts
+++ src/data/Resumes.ts
@@ -24,7 +24,7 @@ const Resumes: ResumesType[] = [
     skills: ["Laravel", "Vue Js", "Cyfirma API", "Socket IO", "Mysql"],
     link: "https://cyseclms.com/login",
     image: 'lms-1.png',
-    images: ['lms-1.png', 'lms-1.png', 'lms-1.png']
+    images: ['lms-1.png', 'lms-2.png', 'lms-3.png', 'lms-4.png', 'lms-5.png']
   },
   {
     id: 2,
@@ -32,8 +32,8 @@ const Resumes: ResumesType[] = [
     description: "Consultancy platform for electricity engineering with PLN engineers via chat, Zoom, and onsite meetings. Integrated with Firebase, Zoom API, and Iconpay payment gateway, using Socket IO for real-time status updates.",
     skills: ["Laravel", "Vue Js", "Firestore", "Socket IO", "Zoom API", "Postgresql", "Iconpay"],
     link: "https://konsultrik.plne.co.id/",
-    image: 'konsultrik-1.png',
-    images: ['konsultrik-1.png', 'konsultrik-1.png', 'konsultrik-1.png']
+    image: 'konsultrik-11.jpg',
+    images: ['konsultrik-11.jpg', 'konsultrik-2.png', 'konsultrik-3.jpg']
   },
   {
     id: 3,
@@ -41,7 +41,7 @@ const Resumes: ResumesType[] = [
     description: "SSO server and threat detection system integrated with F5 API for monitoring unusual traffic and sending notifications via Telegram API.",
     skills: ["Laravel", "Vue Js", "Mysql", "F5 API"],
     image: 'sso-1.png',
-    images: ['sso-1.png', 'sso-1.png', 'sso-1.png']
+    images: ['sso-1.png', 'sso-2.png', 'sso-3.png']
   },
   {
     id: 4,
@@ -50,7 +50,7 @@ const Resumes: ResumesType[] = [
     skills: ["Nest JS", "Angular", "SQL Server", "Ionic"],
     link: "https://tinyurl.com/36j8b2ta",
     image: 'solace-1.jpg',
-    images: ['solace-1.jpg', 'solace-1.jpg', 'solace-1.jpg']
+    images: ['solace-1.jpg', 'solace-2.png', 'solace-3.jpg']
   },
   {
     id: 5,
@@ -59,7 +59,7 @@ const Resumes: ResumesType[] = [
     skills: ["Nest JS", "Angular", "Ionic", "SQL Server"],
     link: "https://tinyurl.com/3tu3rcfw",
     image: 'inventory-1.png',
-    images: ['inventory-1.png', 'inventory-2.png']
+    images: ['inventory-1.png']
   },
   {
     id: 6,
@@ -68,7 +68,7 @@ const Resumes: ResumesType[] = [
     skills: ["Nest JS", "Angular", "Mysql"],
     link: "https://github.com/Ihpaz/operationfrontend",
     image: 'operations-1.png',
-    images: ['operations-1.png', 'operations-1.png', 'operations-1.png']
+    images: ['operations-1.png']
   },
   {
     id: 7,
@@ -76,7 +76,7 @@ const Resumes: ResumesType[] = [
     description: "Create Booking Flight API for E-Travel APP PLN ICON integrated with Drupadi API",
     skills: ["Spring Boot", "SQL Server"],
     image: 'fligh-1.png',
-    images: ['fligh-1.png', 'fligh-1.png', 'fligh-1.png']
+    images: ['fligh-1.png']
   },
   {
     id: 8,
@@ -84,34 +84,32 @@ const Resumes: ResumesType[] = [
     description: "ERP system for palm oil plantations including HRIS, accounting, sales, inventory, and weighbridge system.",
     skills: ["Codeigniter", "PHP", "Postgresql", "VB", "Postgresql"],
     image: 'siap-1.jpg',
-    images: ['siap-1.jpg', 'siap-1.jpg', 'siap-1.jpg']
+    images: ['siap-1.jpg']
   },
   {
     id: 9,
     title: "Web for Managing New IKN PLN Building Opening Guests",
     description: "Web application for managing guest lists, rundown, panoramic and 3D views for the new IKN PLN building opening.",
     skills: ["Laravel", "Vue Js", "Mysql"],
-    link: "https://dev-ikn.air.id",
     image: 'ikn-1.png',
-    images: ['ikn-1.png', 'ikn-1.png', 'ikn-1.png']
+    images: ['ikn-1.png', 'ikn-2.png', 'ikn-3.png']
   },
   {
     id: 10,
     title: "EMS (Electricity Monitoring System)",
     description: "Electricity Monitoring System for monitoring electricity usage , outage , billing etc.",
     skills: ["Spring Boot", "SQL Server"],
-    link: "https://dev-ikn.air.id",
     image: 'ems-1.png',
-    images: ['ems-1.png', 'ems-1.png', 'ems-1.png']
+    images: ['ems-1.png', 'ems-2.png']
   },
   {
     id: 11,
     title: "Portofolio Website",
     description: "My portofolio website was build using React Js with react-router, react-query , redux-toolkit and tailwindcss",
     skills: ["React Js"],
-    link: "https://dev-ikn.air.id",
+    link: "https://ihfazm.vercel.app",
     image: 'portofolio-1.png',
-    images: ['portofolio-1.png', 'portofolio-1.png', 'portofolio-1.png']
+    images: ['portofolio-1.png']
   },
   {
     id: 12,
@@ -119,7 +117,7 @@ const Resumes: ResumesType[] = [
     description: "Dashboard for monitoring KFC data and integrating it with Netsuite, including real-time data synchronization and analytics.",
     skills: ["Spring Boot", "Angular", "Netsuite API", "Oracle"],
     image: 'apintegrasi2.png',
-    images: ['apintegrasi2.png', 'apintegrasi2.png', 'apintegrasi2.png']
+    images: ['apiintegrasi1.png', 'apintegrasi2.png']
   },
 ];
 
